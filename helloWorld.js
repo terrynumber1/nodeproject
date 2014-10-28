026d069b583a578593e97121ccffba36e8c3d813
Andy <andy@email.com>
2014-10-28 05:51:43 -0400

    Page 16 added logo.jpg

--- helloWorld.js
+++ helloWorld.js
@@ -1,5 +1,22 @@
 var http = require('http');
 
+	fs = require('fs');
+
+	function serveStaticFile(res, path, contentType, responseCode) {
+		if(!responseCode)
+			responseCode = 200;
+
+		fs.readFile(__dirname + path, function(err, data) {
+			if(err) {
+				res.writeHead(500, {'Content-Type' : 'text/plain' });
+				res.end('500 - Internal Error');
+			} else {
+				res.writeHead(responseCode, {'Content-Type' : 'contentType'} );
+				res.end(data);
+			}
+		});
+	} // function
+
 http.createServer( function(req, res) {
 
 	// normailzed url by removing querystring, trailing slash, and making it lowercase
@@ -14,6 +31,10 @@ http.createServer( function(req, res) {
 		case '/about':
 			res.writeHead(200, {'Content-Type': 'text/plain'} );
 			res.end('About');
+			break;
+
+		case '/img/logo.jpg':
+			serveStaticFile(res, '/public/img/logo.jpg', 'image/jpeg');
 			break;
 
 		default:
