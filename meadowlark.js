1a6dba3b0dd93188fd97150837e4b2ec5505504c
Andy <Andy@mail.com>
2014-11-01 04:22:23 -0400

    Added folders views/layouts and .handlebars files

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,11 @@
 var express = require('express');
-
 var app = express();
+
+// set up handlebars view engine
+var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
