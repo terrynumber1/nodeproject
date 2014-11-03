1ea2b3032b79adb4178b7d2c70a9cbbd45a86826
Andy <Andy@mail.com>
2014-11-03 08:24:44 -0500

    Added sections 'head' 'jquery' and it's not working, at page 80

--- meadowlark/meadowlark.js
+++ meadowlark/meadowlark.js
@@ -6,7 +6,18 @@ var app = express();
 var fortuneModule = require('./library/fortune.js');
 
 // set up handlebars view engine
-var handlebars = require('express3-handlebars').create({ defaultLayout:'main' });
+var handlebars = require('express3-handlebars').create({
+	defaultLayout:'main',
+	helpers: {
+		section: function(name, options){
+			if(!this._sections) this._sections = {};
+			this._sections[name] = options.fn(this);
+			return null;
+		}
+	}
+});
+
+
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
