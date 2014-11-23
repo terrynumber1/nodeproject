9d710056bc46eca0254e6bf2afa23949cc3f351e
Andy <Andy@mail.com>
2014-11-23 06:21:03 -0500

    added code on page 144

--- meadowlark/meadowlark.js
+++ meadowlark/meadowlark.js
@@ -155,6 +155,53 @@ app.post('/contest/vacation-photo/:year/:month', function(req, res) {
 	})
 })
 
+// make sure data directory exists
+var dataDir = __dirname + '/data';
+var vacationPhotoDir = dataDir + '/vacation-photo';
+
+fs.existsSync(dataDir) || fs.mkdirSync(dataDir);
+fs.existsSync(vacationPhotoDir) || fs.mkdirSync(vacationPhotoDir);
+
+function saveContestEntry(contestName, email, year, month, photoPath) {
+	// TODO.. this will come later
+}
+
+app.post('/contest/vacation-photo/:year/:month', function(req, res) {
+	var form = new formidable.IncomingForm();
+	form.parse(req, function(err, fields, files) {
+		if(err)
+			return res.redirect(303, '/error');
+
+		if(err) {
+			res.session.flash = {
+				type: 'danger',
+				intro: 'Oops!',
+				message: 'There was an error processing your submission. '
+					+ 'Please try again',
+			}; // res.session.flash
+			return res.redirect(303, '/contest/vacation-photo');
+		} // if
+
+		var photo = files.photo;
+		var dir = vacationPhotoDir + '/' + Date.now();
+		var path = dir + '/' + photo.name;
+
+		fs.mkdirSync(dir);
+		fs.renameSync(photo.path, dir + '/' + photo.name);
+
+		saveContestEntry('vacation-photo', fields.email, req.params.year, 
+			req.params.month, path);
+
+		req.session.flash = {
+			type: 'sucess',
+			intro: 'Good luck!',
+			message: 'You have been entered into the contest.',
+		};
+		return res.redirect(303, '/contest/vacation-photo/entries');
+	});
+
+});
+
 // ====================================
 // ====================================
 
